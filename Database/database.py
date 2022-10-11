5a53cf6291a3bbdeeb98986164ddf5fc7d94cde9
Tapanguru1 <tapan.guru@in.ey.comcleaeclear>
2022-10-11 17:11:19 +0530

    updated for git

--- Database/database.py
+++ Database/database.py
@@ -4,18 +4,20 @@ from sqlalchemy.orm import sessionmaker
 import os
 import urllib
 
-host_server = os.environ.get('host_server','fishpractice.mysql.database.azure.com' )
-db_server_port = urllib.parse.quote_plus(str(os.environ.get('db_server_port', 3306)))
+host_server = os.environ.get('host_server','localhost')
+db_server_port = urllib.parse.quote_plus(str(os.environ.get('db_server_port', '3306')))
 database_name = os.environ.get('database_name', 'fishpractice')
-db_username = urllib.parse.quote_plus(str(os.environ.get('db_username', 'tapanguru12@fishpractice')))
-db_password = urllib.parse.quote_plus(str(os.environ.get('db_password', 'Fish@123')))
+db_username = urllib.parse.quote_plus(str(os.environ.get('db_username', 'tapan')))
+db_password = urllib.parse.quote_plus(str(os.environ.get('db_password', '1234')))
 SQLALCHEMY_DATABASE_URL='mysql+mysqlconnector://{}:{}@{}:{}/{}'.format(db_username,db_password,host_server,db_server_port,database_name)
 
 #SQLALCHEMY_DATABASE_URL='sqlite:///./product.db'
 
-engine= create_engine(SQLALCHEMY_DATABASE_URL,pool_size=3,max_overflow=0
+engine= create_engine(SQLALCHEMY_DATABASE_URL,pool_size=3,max_overflow=0)
 #connect_args={"check_same_thread":False}
-)
+
+
+
 
 SessionLocal=sessionmaker(bind=engine,autocommit=False,autoflush=False)
 Base=declarative_base()
--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from cgi import test
 import numpy as np
 from fastapi import FastAPI,Depends,Query
 from Database.database import engine 
@@ -6,38 +7,32 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression 
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from pydantic import BaseModel
 
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException,status,Request,Form
 import models
 from Database.database import engine 
 
-
-models.Base.metadata.create_all(engine)
-
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
 
 from Database.database import SessionLocal
 app=FastAPI()
-@app.get('/')
-def index():
-    return {'message': 'Hello World'}
-class request_body(BaseModel):
-    Weight: float
-    Length1:float
-    Length2:float
-    Length3: float
-    Height: float
-    Width:float
 
-
+models.Base.metadata.create_all(engine)
 
 dataset=pd.read_csv("fish.csv")
 dataset.head()
 X=dataset.iloc[:,1:7].values
 y=dataset.iloc[:,0].values
-X_train, X_test,y_train,y_test=train_test_split(X,y,test_size=0.5,random_state=0)
-regression=LogisticRegression(solver='lbfgs',max_iter=1000)
-regression.fit(X_train,y_train)
+
+templates = Jinja2Templates(directory="templates")
+#X_train, X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=0)
+regression=LinearDiscriminantAnalysis(solver='svd',n_components=1)
+regression.fit(X,y)
 def get_db():
     try:
         db=SessionLocal()
@@ -45,10 +40,51 @@ def get_db():
     finally:
         db.close()
 
+app.mount("/templates/static", StaticFiles(directory="templates/static"), name="static")
 
+@app.get("/frontend", response_class=HTMLResponse)
+async def read_item(request: Request):
+    weight = ""
+    length1=""
+    length2=''
+    length3=''
+    height=''
+    width=''
+    species=''
+
+    return templates.TemplateResponse("item.html",context={'request': request, 'weight': weight,'length1':length1,'length2':length2,'length3':length3,'height':height,'width':width,'species':species})
+
+@app.post("/frontend")
+def form_post(request: Request, Weight: float = Form(...),Length1:float=Form(...),Length2:float=Form(...),Length3:float=Form(...),Height:float=Form(...),Width:float=Form(...)):
+    weight =Weight
+    length1=Length1
+    length2=Length2
+    length3=Length3
+    height=Height
+    width=Width
+    test_data = [[weight, length1, length2,length3, height, width]]
+    class_idx=regression.predict(test_data)[0]
+    species=class_idx
+    
+    return templates.TemplateResponse('item.html', context={'request': request, 'weight': weight,'length1':length1,'length2':length2,'length3':length3,'height':height,'width':width,'species':species})
+
+
+@app.get('/')
+def index():
+    return {'message': 'Hello World'}
+
+class logistic(BaseModel):
+    Weight: float
+    Length1: float
+    Length2: float
+    Length3: float
+    Height: float
+    Width: float
+    Species:str
+    
 
 @app.post('/predict')
-def predict(data : request_body, db: Session= Depends(get_db)):
+def predict(data: logistic, db: Session= Depends(get_db)):
     test_model=models.Product()
     test_data=[[
         data.Weight,
@@ -58,6 +94,7 @@ def predict(data : request_body, db: Session= Depends(get_db)):
         data.Height,
         data.Width]]
     y_pred=regression.predict(test_data)[0]
+    data.Species=y_pred
     test_model.Species=y_pred
     test_model.Weight=data.Weight
     test_model.Length1=data.Length1
@@ -67,4 +104,5 @@ def predict(data : request_body, db: Session= Depends(get_db)):
     test_model.Width=data.Width
     db.add(test_model)
     db.commit()
+    #db.refresh(test_model)
     return y_pred
